940289cb85dd5a4382673a63d0a7ee4b0be813d7
Amirah Kabir <maiza1497@gmail.com>
2018-10-10 18:34:17 +0000

    Task 2a complete

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ console.log(heading.innerText);
 heading.innerHTML = "<h3> Text changed </h3>";
 
 /* Task 2 using plain JavaScript */
-
-
+//Task 2a
+var textBtn = document.getElementById('addText2');
+textBtn.addEventListener('click', function(event){
+    var task2 = document.getElementById('task2a');
+    var paragraph = document.createElement('p');
+    paragraph.innerText = "Hello World";
+    task2.appendChild(paragraph);
+});
 
 /* Task 4 using jQuery */
