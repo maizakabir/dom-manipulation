b8e137ba7913147511fe823b458e5ccefce04dd0
Amirah Kabir <maiza1497@gmail.com>
2018-10-10 18:07:47 +0000

    Task 1 complete

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 /* Task 1 */
+var heading = document.getElementById('heading');       
+console.log(heading.innerText);
 
+heading.innerHTML = "<h3> Text changed </h3>";
 
 /* Task 2 using plain JavaScript */
 
