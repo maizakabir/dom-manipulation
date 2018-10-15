00981177b8d5f5a0f61236c34d490e70f0ce75c7
Amirah Kabir <maiza1497@gmail.com>
2018-10-15 03:33:30 +0000

    Task 4 complete

--- script.js
+++ script.js
@@ -41,3 +41,15 @@ $('#addText4').click (function (event){
     var paragraph= $('<p>').text('Hello World');
     $('#task4a').append(paragraph);
 });
+
+//Task 4b
+function changeBG (event){
+    var color= event.target.innerText.toLowerCase();
+    $('body').css('background-color', color);
+}
+
+//Task 4c
+$("#sum").click(function(event){
+    var sum= Number($("#num1").val()) + Number($("#num2").val());
+    $('#result').text(sum);
+});
